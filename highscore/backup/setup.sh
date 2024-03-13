d0ff2c887c0b37b4a1953ba8a5515ee030f6afa3
A.Davarski <42774466+adavarski@users.noreply.github.com>
2024-03-13 08:26:10 +0200

    Update setup.sh

--- highscore/backup/setup.sh
+++ highscore/backup/setup.sh
@@ -4,8 +4,8 @@ set -x
 
 oc apply -f 01-namespace.yaml
 oc project arcade
-oc new-app --name=game --context-dir=game https://github.com/adavarski/s3e
-oc new-app --name=highscore --context-dir=highscore https://github.com/adavarski/s3e
+oc new-app --name=game --context-dir=game https://github.com/adavarski/OpenShift-CRC-Playground
+oc new-app --name=highscore --context-dir=highscore https://github.com/adavarski/OpenShift-CRC-Playground
 DOMAIN=$(oc get ingresses.config.openshift.io  cluster -o jsonpath='{.spec.domain}')
 oc expose svc game --path=/s3e --hostname=arcade.$DOMAIN
 oc expose svc highscore --path=/highscore --hostname=arcade.$DOMAIN
